0fe7e92c0c5bf08dfea9604f049a8b46cb1d621e
Diti Arora <diti.aromatic@gmail.com>
2023-04-02 02:17:40 +0530

    it is working, and also not

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { auth } from "./firebase";
 import { db } from "./firebase";
-import { collection, setDoc, doc, getDocs } from "firebase/firestore"; 
+import { collection, setDoc, doc, getDocs } from "firebase/firestore";
 
 const App = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [userr, setUser] = useState({});
+  const [data, setData] = useState([]);
+  const [inp, setInp] = useState();
 
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [userr, setUser] = useState({})
+  const getData = async () => {
+      await getDocs(collection(db, "users", userr.uid)).then((querySnapshot) => {
+        querySnapshot.forEach((element) => {
+          var dataa = element.data();
+          setData([dataa, ...data]);
+          console.log(data);
+        });
+      });
+  };
 
-  const [name, setName] = useState("")
-
-  
   const addDoc = async (du) => {
-    // console.log(userr);
     try {
       const docRef = await setDoc(doc(db, "users", du), {
         email: email,
@@ -23,46 +33,57 @@ const App = () => {
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  }
+  };
 
-// sign up
+  // sign up
   const handleSubmit = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      setUser(user.user);
+      addDoc(userr.uid);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-   try {
-    const user =  await createUserWithEmailAndPassword(auth, email, password)
-    setUser(user)
-    addDoc(user.user.uid)
-  }
-  catch(error) {
-    console.log(error.message);
-  }
-  
-} 
-
-useEffect(() => {
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser)
-    console.log(userr);
-  })
-  }, [userr])
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(userr);
+    });
+  }, [userr]);
 
   return (
     <>
       <div className="signupdiv">
-      <p>Email</p>
-        <input value={email} onChange={e => setEmail(e.target.value)}  type="text"/>
+        <p>Email</p>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+        />
 
         <p className="pas">Password</p>
-        <input value={password} onChange={e => setPassword(e.target.value)}  type="text"/>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+        />
 
         <br />
         <button onClick={handleSubmit}>Sign up</button>
       </div>
 
       <div className="dbdiv">
-        {/* <input placeholder="Name" value={name} onChange={e => setName(e.target.value)}  type="text"/> <br /> */}
-        <button>retrieve data</button>
+        {/* <input
+          value={inp}
+          onChange={(e) => setInp(e.target.value)}
+          placeholder="Email"
+          type="text"
+        /> */}
+        <br />
+        <button onClick={getData}>retrieve data</button>
+
       </div>
     </>
   );
@@ -70,4 +91,3 @@ useEffect(() => {
 
 export default App;
 
-// 
